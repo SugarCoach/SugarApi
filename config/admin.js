9888699942e8bbace33883dc4f4deebf6db84e23
Rama <ramirocarnicersouble8@gmail.com>
2023-10-27 08:44:00 -0300

    JWT Secret agregado

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET',"wP6umpoC6/UiVqXyeyN8cg=="),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
