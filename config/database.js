7fe63196b2353ebae819c27dff067da1ee581886
Rama <ramirocarnicersouble8@gmail.com>
2023-12-05 09:41:46 -0300

    Installed pg, aws-S3 upload provider and updated the config files

--- config/database.js
+++ config/database.js
@@ -2,6 +2,22 @@ const path = require('path');
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL)
 
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
+    },
+    useNullAsDefault: true,
+  },
+});
+
+/*
 module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'postgres');
 
@@ -91,4 +107,4 @@ module.exports = ({ env }) => {
       acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
     },
   };
-};
+};*/
