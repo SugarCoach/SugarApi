12c54f19df54b41c2620eaf5a03c159cdb85d3b1
Rama <ramirocarnicersouble8@gmail.com>
2023-12-20 14:18:14 -0300

    Server.js AppKeys fixed

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', "SeqstsKofIWNsUPrK95ZQQ==,iP+MLK9dZ8hEd8blPRFENw==,rcm1NFt7RIlQfzDVpGWwmw==,UDhz02ffQSnrLKGARZLQ5g=="),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
