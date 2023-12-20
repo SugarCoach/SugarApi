0bf0448e7fee38ecf1acf2117db5222027bdfd7c
Rama <ramirocarnicersouble8@gmail.com>
2023-12-20 13:06:27 -0300

    Database config file changed to default

--- config/database.js
+++ config/database.js
@@ -6,6 +6,19 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
+    },
+    useNullAsDefault: true,
+  },
+});/*
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
       host: "sugar-backendapp-dbinstance-cnih3mh6qrnl.cmoghtwgghmz.us-east-1.rds.amazonaws.com",
       port: 5432,
       database: "sugar-backendapp-dbinstance-cnih3mh6qrnl",
@@ -14,7 +27,7 @@ module.exports = ({ env }) => ({
     },
     useNullAsDefault: true,
   },
-});
+});*/
 /*
 module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'mysql');
