e117f564a962f4fcb784abf048af0604a9474257
Rama <ramirocarnicersouble8@gmail.com>
2023-12-20 14:30:25 -0300

    Admin.js Token_Salt and Tranfer_Salt

--- config/admin.js
+++ config/admin.js
@@ -3,11 +3,11 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET',"wP6umpoC6/UiVqXyeyN8cg=="),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', "9wDIACYJfzDrWp/U+xyW+Q=="),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', "YYf64y4kejyHGauobjUvZA=="),
     },
   },
   flags: {
