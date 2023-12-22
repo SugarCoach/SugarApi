26713605e2c1c2336caca47bb7b6221b196cde56
Rama <ramirocarnicersouble8@gmail.com>
2023-12-22 09:08:51 -0300

    Server.js Host changed to 0.0.0.0

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '3.83.111.70'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS', ["SeqstsKofIWNsUPrK95ZQQ==","iP+MLK9dZ8hEd8blPRFENw==","rcm1NFt7RIlQfzDVpGWwmw==","UDhz02ffQSnrLKGARZLQ5g=="]),
