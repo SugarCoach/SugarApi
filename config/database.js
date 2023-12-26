21930891899145f53f5731d79355fe8e2d7c5c08
Rama <ramirocarnicersouble8@gmail.com>
2023-12-26 08:52:37 -0300

    Adding env DB credentials

--- config/database.js
+++ config/database.js
@@ -19,11 +19,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "sugar-backendarchitecture-dbinstance-v4h4cm75jlv9.cmoghtwgghmz.us-east-1.rds.amazonaws.com"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+      database: env("DATABASE_NAME", "-"),
+      user: env("DATABASE_USERNAME", "sugarDb"),
+      password: env("DATABASE_PASSWORD", "Sugarcoach2023!"),
     },
     useNullAsDefault: true,
   },
