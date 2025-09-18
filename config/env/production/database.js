07b0d26ded25a78b8f2db5c3819303891e0b3853
SugarCoach <87439863+SugarCoach@users.noreply.github.com>
2025-09-17 22:14:34 -0300

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,11 @@
-
+// config/env/production/database.js
 module.exports = ({ env }) => ({
-	connection: {
-		client: 'postgres',
-		connection: {
-			host: env('DATABASE_HOST'),
-			port: env.int('DATABASE_PORT'),
-			database: env('DATABASE_NAME'),
-			user: env('DATABASE_USERNAME'),
-			password: env('DATABASE_PASSWORD'),
-			ssl: {
-				rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-			},
-		},
-		debug: false,
-	},
+  connection: {
+    client: 'postgres',
+    connection: {
+      connectionString: env('DATABASE_URL'),
+      ssl: { rejectUnauthorized: false },
+    },
+    pool: { min: 0, max: 10 },
+  },
 });
