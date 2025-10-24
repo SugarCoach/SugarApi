74a4ae612586832ba9748968424ba56e88acb4f1
Victoria Valli <Vallivictoria@gmail.com>
2025-10-24 11:27:56 -0300

    Agregue la API token-qr con esquema, controlador, enrutador y servicio; actualice el esquema de datos de usuario y las definiciones de TypeScript

---
+++ src/api/token-qr/routes/token-qr.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * token-qr router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::token-qr.token-qr');
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -406,6 +406,42 @@ export interface ApiDailyRegisterDailyRegister extends Schema.CollectionType {
   };
 }
 
+export interface ApiTokenQrTokenQr extends Schema.CollectionType {
+  collectionName: 'token_qrs';
+  info: {
+    singularName: 'token-qr';
+    pluralName: 'token-qrs';
+    displayName: 'Token QR';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    tokenTemporal: Attribute.Text;
+    expireTime: Attribute.BigInteger;
+    user_datum: Attribute.Relation<
+      'api::token-qr.token-qr',
+      'oneToOne',
+      'api::user-data.user-data'
+    >;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::token-qr.token-qr',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::token-qr.token-qr',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface ApiTreatmentTreatment extends Schema.CollectionType {
   collectionName: 'treatments';
   info: {
@@ -477,6 +513,13 @@ export interface ApiUserDataUserData extends Schema.CollectionType {
       'plugin::users-permissions.user'
     >;
     sugar_points: Attribute.Integer;
+    account_type: Attribute.String;
+    nickname: Attribute.String;
+    token_qr: Attribute.Relation<
+      'api::user-data.user-data',
+      'oneToOne',
+      'api::token-qr.token-qr'
+    >;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     createdBy: Attribute.Relation<
@@ -831,6 +874,7 @@ declare module '@strapi/types' {
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
       'api::daily-register.daily-register': ApiDailyRegisterDailyRegister;
+      'api::token-qr.token-qr': ApiTokenQrTokenQr;
       'api::treatment.treatment': ApiTreatmentTreatment;
       'api::user-data.user-data': ApiUserDataUserData;
       'plugin::upload.file': PluginUploadFile;
